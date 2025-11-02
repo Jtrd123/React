11bb7bd71fb8e63a550ff70371111e639ce9cf1c
Jtrd123 <06604@sukhon.ac.th>
2025-11-02 17:08:25 +0700

    Connect Form

--- my-app/src/component/AddForm.jsx
+++ my-app/src/component/AddForm.jsx
@@ -4,9 +4,14 @@ import { useState } from "react"
 export default function AddForm() {
   const [name, setName] = useState("")
   const [gender, setGender] = useState("")
+
+  function saveData(e) {
+    e.preventDefault()
+    console.log( name, gender )
+  }
   return (
     <section className="container">
-      <form>
+      <form onSubmit={saveData}>
         <label>ชื่อปะชากร</label>
         <input type="text" value={name} onChange={(e) => setName(e.target.value)}/>
         <select value={gender} onChange={(e) => setGender(e.target.value)}>
